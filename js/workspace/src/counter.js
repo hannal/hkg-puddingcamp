bb193d618f135be4b1826dab0edb7283092f18a6
Kay Cha <kay@hannal.net>
2023-08-29 16:03:32 +0900

    Fix: 사소한 es6 api 사용하는 코드

--- js/workspace/src/counter.js
+++ js/workspace/src/counter.js
@@ -1,9 +1,10 @@
 export function setupCounter(element) {
-  let counter = 0
+  let counter = 0;
   const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+    console.log('includes', count.toString().includes('hannal'));
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+  element.addEventListener('click', () => setCounter(counter + 1));
+  setCounter(0);
 }
